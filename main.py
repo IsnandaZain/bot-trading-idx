2dc683bcfaddc614037822a8c818d2e9f0268161
00345976 <isnandamzain@corp.bri.co.id>
2025-12-03 09:55:54 +0700

    update main code

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ import config.configuration as cfg
 import pandas as pd
 import sqlalchemy
 
+from src.spreadsheet import migrate
+
 from src.strategy import build_trading_state as bts
 from src.strategy import buy_signal as bsg
 from src.strategy import trade_plan as tp
+
+from src.tracking import log_trade as lt
+from src.tracking import performance_analyzer as pa
 
 def start():
     # create db_conn and db_session for pandas
@@ -31,7 +36,7 @@ def start():
         # get data histories
         query = """
             SELECT * FROM data_histories 
-            WHERE ticker = '{ticker}' AND date <= '2025-05-19'
+            WHERE ticker = '{ticker}' AND date <= '2025-06-26'
             ORDER BY date
         """.format(ticker=ticker.ticker)
         df = pd.read_sql(query, db_conn)
@@ -46,7 +51,9 @@ def start():
         if should_buy:
             print(f"✅ BUY SIGNAL for {ticker.ticker}: {signal_name} | {reason}")
             trade_plan = tp.generate_trade_plan(trading_state, signal_name)
-            print(trade_plan)
+            
+            # save trade_log
+            lt.log_trade_decision(trading_state, trade_plan)
 
         else:
             print(f"❌ No Buy Signal for {ticker.ticker}: {reason}")
@@ -54,6 +61,10 @@ def start():
 
         end = time.time()
         print(f"time elapsed for ticker {ticker.ticker} is {end - start}")
+
+    realtime_data = migrate.read(save=False)
+    realtime_data = pd.DataFrame(realtime_data)
+    pa.update_trade_logs(realtime_data)
 
     exit(1)
 
